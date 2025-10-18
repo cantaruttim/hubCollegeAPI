2e4f32b2a13b38658375d85f274a8abdc355cb93
cantaruttim <cantaruttim@outlook.com>
2025-10-18 12:23:45 -0300

    controller

--- Controllers/FormsActivityController.cs
+++ Controllers/FormsActivityController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HubCollege.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HubCollege.Controllers
@@ -10,6 +11,13 @@ namespace HubCollege.Controllers
     [Route("api/[controller]")]
     public class FormsActivityController : ControllerBase
     {
-        
+        // accesing db
+        private readonly AppDbContext _appDbContext;
+
+        // constructor
+        public FormsActivityController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;    
+        }
     }
 }

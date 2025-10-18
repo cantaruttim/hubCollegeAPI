0604408f514bc38b8d6a6d328faf72624df78f3b
cantaruttim <cantaruttim@outlook.com>
2025-10-18 12:49:15 -0300

    connection ef

--- obj/Debug/net9.0/HubCollege.AssemblyInfo.cs
+++ obj/Debug/net9.0/HubCollege.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HubCollege")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b8d20007d6ddbcfe4963397c01b2639b038b40a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19e0bbb6d40578eba797f168fbe7ef2ab11ad965")]
 [assembly: System.Reflection.AssemblyProductAttribute("HubCollege")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HubCollege")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

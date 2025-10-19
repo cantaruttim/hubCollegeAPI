f19de7894d8ed29c84d23a15744a4ed657796bfa
cantaruttim <cantaruttim@outlook.com>
2025-10-19 10:40:03 -0300

    data anotation

--- model/FormsActivity.cs
+++ model/FormsActivity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace HubCollege.model
 {
     public class FormsActivity
     {
+        [Key]
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "registrationNumber is Required")]
         public string registrationNumber { get; set; }
+        
+        [Required(ErrorMessage = "Name is Required")]
         public string name { get; set; }
 
+        [Required(ErrorMessage = "Email is Required")]
         public string email { get; set; }
 
         public string questionOne { get; set; }

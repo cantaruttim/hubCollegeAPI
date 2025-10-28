7cac5f1572937effc0363bd72ce198ce3844e72b
cantaruttim <cantaruttim@outlook.com>
2025-10-28 00:18:01 -0300

    try:catch get activities

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,19 @@
 const activityList = document.getElementById('activity-list');
 const apiURL = 'http://localhost:5001/api/formsActivity';
 
-const fetchFActivityAPI = fetch(
-    apiURL,
-    {   
-        method: 'GET', 
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-).then((response) => response.json())
- .then((data) => console.log(data));
 
+const getActivityList = async () => {
+    try {
+        const response = await fetch(
+            apiURL,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    } catch (error) {
+        console.error('Error fetching activity list:', error);
+    }
+}

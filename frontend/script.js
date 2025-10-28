a837c70ca5f37c9b5da942887196557f6110a959
cantaruttim <cantaruttim@outlook.com>
2025-10-28 00:26:53 -0300

    activity list

--- frontend/script.js
+++ frontend/script.js
@@ -13,7 +13,21 @@ const getActivityList = async () => {
                 }
             }
         );
+        if (!response.ok) {
+            throw new Error('Erro while fetching activity list');
+        }
+
+        const activities = await response.json();
+
+        activities.foreach( (activity) => {
+            const newLi = document.createElement('li');
+            newLi.innerText = `Student: ${activity.nome}`;
+            activitiesList.appendChild(newLi);
+        })
+
     } catch (error) {
-        console.error('Error fetching activity list:', error);
+        console.error(error);
     }
 }
+
+getActivityList();

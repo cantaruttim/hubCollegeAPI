ef54df0eb816894666f4fb6f34c3bd3349d838b7
cantaruttim <cantaruttim@outlook.com>
2025-11-09 12:23:16 -0300

    Methods to grade

--- model/Interfaces/Grades.cs
+++ model/Interfaces/Grades.cs
@@ -7,6 +7,42 @@ namespace HubCollege.model.Interfaces
 {
     public interface Grades
     {
+        public int somaAtividades()
+        {
+            /* 
+                sum activities by student
+                total = 10
+            */
+            return 0;
+        }
+
+        public int somaProva()
+        {
+            /* 
+                sum tests by student
+                total = 3
+            */
+            return 0;
+        }
+
+        public int somaRecuperacao()
+        {
+            // sum recovery test by student
+            return 0;
+        }
         
+        public int calcularMedia()
+        {
+            /*
+                calculate average grade by student
+                average = (sum activities + sum tests)
+
+                if average >= 7
+                    return "Approved"
+                else
+                    return "Reproved" 
+            */
+            return 0;
+        }
     }
 }

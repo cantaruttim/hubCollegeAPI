c5930bda56ba44f93a310eb635ce8d44e7a4ccff
cantaruttim <cantaruttim@outlook.com>
2025-11-09 16:42:43 -0300

    updated Project

---
+++ model/Perfil/Permission.cs
@@ -0,0 +1,12 @@
+
+namespace HubCollege.model.Perfil
+{
+    public class Permission
+    {
+
+        public int Id { get; set; }
+        public string PermissionName { get; set; } // Adm, User or Teacher
+        public string Description { get; set; } // Role
+        
+    }
+}

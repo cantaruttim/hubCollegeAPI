1fbf865544621d4cd6195fcfd31d7cadd11b9314
cantaruttim <cantaruttim@outlook.com>
2025-11-09 16:30:35 -0300

    updated

--- model/Courses.cs
+++ model/Courses.cs
@@ -6,7 +6,7 @@ namespace HubCollege.model
 
         public int Id { get; set; }
         public string CourseName { get; set; }
-        
+
         // public string CourseDetail { get; set; }
         public string CourseDuration { get; set; } // one year, 6 months or 2 weeks
 
--- model/Students.cs
+++ model/Students.cs
@@ -3,6 +3,13 @@ namespace HubCollege.model
 {
     public class Students : Interfaces.IGrades
     {
+
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentPhone { get; set; }
+        public string StudentCourse { get; set; }
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
         
     }
 }

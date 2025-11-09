8fb6bc2eb0ab2d71173678fe6616ee424f274bb1
cantaruttim <cantaruttim@outlook.com>
2025-11-09 16:27:04 -0300

    Class Course

--- model/Courses.cs
+++ model/Courses.cs
@@ -3,6 +3,13 @@ namespace HubCollege.model
 {
     public class Courses
     {
+
+        public int Id { get; set; }
+        public string CourseName { get; set; }
         
+        // public string CourseDetail { get; set; }
+        public string CourseDuration { get; set; } // one year, 6 months or 2 weeks
+
+
     }
 }

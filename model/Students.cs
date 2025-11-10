7ef26c194f58cb6a830114490875790e81313bcb
cantaruttim <cantaruttim@outlook.com>
2025-11-09 21:06:05 -0300

    updated Student

--- model/Students.cs
+++ model/Students.cs
@@ -1,17 +1,37 @@
 
+using HubCollege.model.Perfil;
+
 namespace HubCollege.model
 {
-    public class Students : Interfaces.IGrades
+    public class Students
     {
 
         public string StudentRegistration { get; set; }
         public string StudentName { get; set; }
         public string StudentPhone { get; set; }
         public string StudentCourse { get; set; }
+
+        public Permission PermissionLevel { get; set; }
+
         public DateTime StartAt { get; set; }
         public DateTime EndAt { get; set; }
 
         public Students() { }
+
+        public Students(
+            string studentRegistration,
+            string studentName,
+            string permissionLevel
+        )
+        {
+            StudentRegistration = studentRegistration;
+            StudentName = studentName;
+            PermissionLevel = new Permission
+            {
+                PermissionName = permissionLevel
+            };
+        }
+
 
         public Students(
             string studentName,

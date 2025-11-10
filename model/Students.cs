91e5c31ce4ce6f81a77f7fb5a83abdaa0d92ff44
cantaruttim <cantaruttim@outlook.com>
2025-11-09 21:11:07 -0300

    updated Student

--- model/Students.cs
+++ model/Students.cs
@@ -1,22 +1,40 @@
-
 using HubCollege.model.Perfil;
 
 namespace HubCollege.model
 {
     public class Students
     {
-
         public string StudentRegistration { get; set; }
         public string StudentName { get; set; }
         public string StudentPhone { get; set; }
         public string StudentCourse { get; set; }
-
         public Permission PermissionLevel { get; set; }
-
         public DateTime StartAt { get; set; }
         public DateTime EndAt { get; set; }
 
         public Students() { }
+
+        public Students(
+            string studentRegistration,
+            string studentName,
+            string studentPhone,
+            string studentCourse,
+            string permissionLevel,
+            DateTime startAt,
+            DateTime endAt
+        )
+        {
+            StudentRegistration = studentRegistration;
+            StudentName = studentName;
+            StudentPhone = studentPhone;
+            StudentCourse = studentCourse;
+            PermissionLevel = new Permission
+            {
+                PermissionName = permissionLevel
+            };
+            StartAt = startAt;
+            EndAt = endAt;
+        }
 
         public Students(
             string studentRegistration,
@@ -30,8 +48,12 @@ namespace HubCollege.model
             {
                 PermissionName = permissionLevel
             };
-        }
 
+            StudentPhone = string.Empty;
+            StudentCourse = string.Empty;
+            StartAt = DateTime.MinValue;
+            EndAt = DateTime.MinValue;
+        }
 
         public Students(
             string studentName,
@@ -46,7 +68,9 @@ namespace HubCollege.model
             StudentCourse = studentCourse;
             StartAt = startAt;
             EndAt = endAt;
+
+            StudentRegistration = string.Empty;
+            PermissionLevel = new Permission();
         }
-        
     }
 }
